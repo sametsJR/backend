70e28d2ce94a1ce55c2ead4275017827aa301259
Alex Samets <aliaksandr.samets@gmail.com>
2019-10-05 19:07:59 +0300

    Added rules for incomplete quadratic equations. Modified README.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# backend
-Backend for equation calc
+### Backend for equation calculator
+
+Try this api on [Heroku](https://shrouded-mountain-95416.herokuapp.com/)
+
+### How to run locally:
+
+```
+~$ bundle
+~$ rails s
+~$ curl -X POST -d '{"a":1.0,"b":1.0,"c":-6.0,"type":"quadratic"}' -H "Content-Type: application/json" http://localhost:3000/equations
+
+Return: [2.0,-3.0]
+```
+### Examples for quadratic equation
+
+```
+curl -X POST -d '{"a":1.0,"b":1.0,"c":1.0,"type":"quadratic"}' -H "Content-Type: application/json" https://shrouded-mountain-95416.herokuapp.com/equations
+Return: `No solutions`
+```
--- lib/services/calculates/equation.rb
+++ lib/services/calculates/equation.rb
@@ -10,15 +10,31 @@ module Services
 
         def quadratic_equation(equation_params)
           discriminant = equation_params[:b] ** 2 - 4 * equation_params[:a] * equation_params[:c]
+          no_solutions_error = 'No solutions'
+          coefficient_error = "Coefficient a can't be 0"
 
-          if discriminant > 0
+          return coefficient_error if equation_params[:a] == 0
+          return no_solutions_error if equation_params[:b] == 0 && equation_params[:c] != 0 && (-equation_params[:c] / equation_params[:a]) < 0
+          return no_solutions_error if discriminant < 0
+
+          if equation_params[:b] == 0 && equation_params[:c] == 0
+            x1 = 0
+            [x1]
+          elsif equation_params[:b] == 0 && equation_params[:c] != 0 && (-equation_params[:c] / equation_params[:a]) > 0
+            x1 = Math.sqrt(-equation_params[:c] / equation_params[:a])
+            x2 = -Math.sqrt(-equation_params[:c] / equation_params[:a])
+            [x1, x2]
+          elsif equation_params[:b] != 0 && equation_params[:c] == 0
+            x1 = 0
+            x2 = -equation_params[:b] / equation_params[:a]
+            [x1, x2]
+          elsif discriminant > 0
             x1 = (-equation_params[:b] + Math.sqrt(discriminant)) / (2 * equation_params[:a])
             x2 = (-equation_params[:b] - Math.sqrt(discriminant)) / (2 * equation_params[:a])
-          elsif discriminant == 0
+            [x1, x2]
+          else discriminant == 0
             x1 = -equation_params[:b] / (2 * equation_params[:a])
-          else
-            x1 = 'No solutions'
-            x2 = 'No solutions'
+            [x1]
           end
         end
       end
