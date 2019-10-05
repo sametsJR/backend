ece737aa8b26f963ae0ed6c728c368481c49e25b
Alex Samets <aliaksandr.samets@gmail.com>
2019-10-05 20:50:58 +0300

    Added linear_equation method to service. Converted input numbers to float. Modified README

--- README.md
+++ README.md
@@ -7,13 +7,25 @@ Try this api on [Heroku](https://shrouded-mountain-95416.herokuapp.com/)
 ```
 ~$ bundle
 ~$ rails s
+
+For quadratic equation:
 ~$ curl -X POST -d '{"a":1.0,"b":1.0,"c":-6.0,"type":"quadratic"}' -H "Content-Type: application/json" http://localhost:3000/equations
 
 Return: [2.0,-3.0]
+
+For linear equation:
+~$ curl -X POST -d '{"a":0,"b":0,"type":"linear"}' -H "Content-Type: application/json" http://localhost:3000/equations
+Return: Infinite
 ```
 ### Examples for quadratic equation
 
 ```
 curl -X POST -d '{"a":1.0,"b":1.0,"c":1.0,"type":"quadratic"}' -H "Content-Type: application/json" https://shrouded-mountain-95416.herokuapp.com/equations
-Return: `No solutions`
+Return: No solutions
+```
+### Examples for linear equation
+
+```
+curl -X POST -d '{"a":1.0,"b":-2.0,"type":"linear"}' -H "Content-Type: application/json" https://shrouded-mountain-95416.herokuapp.com/equations
+Return: [2.0]
 ```
--- lib/services/calculates/equation.rb
+++ lib/services/calculates/equation.rb
@@ -4,12 +4,13 @@ module Services
       class << self
         def call(equation_params)
           return quadratic_equation(equation_params) if equation_params[:type] == 'quadratic'
+          return linear_equation(equation_params) if equation_params[:type] == 'linear'
         end
 
         private
 
         def quadratic_equation(equation_params)
-          discriminant = equation_params[:b] ** 2 - 4 * equation_params[:a] * equation_params[:c]
+          discriminant = (equation_params[:b]).to_f ** 2 - 4 * (equation_params[:a]).to_f * (equation_params[:c]).to_f
           no_solutions_error = 'No solutions'
           coefficient_error = "Coefficient a can't be 0"
 
@@ -18,22 +19,33 @@ module Services
           return no_solutions_error if discriminant < 0
 
           if equation_params[:b] == 0 && equation_params[:c] == 0
-            x1 = 0
+            x1 = 0.to_f
             [x1]
           elsif equation_params[:b] == 0 && equation_params[:c] != 0 && (-equation_params[:c] / equation_params[:a]) > 0
-            x1 = Math.sqrt(-equation_params[:c] / equation_params[:a])
-            x2 = -Math.sqrt(-equation_params[:c] / equation_params[:a])
+            x1 = Math.sqrt((-equation_params[:c]).to_f / (equation_params[:a]).to_f)
+            x2 = -Math.sqrt((-equation_params[:c]).to_f / (equation_params[:a]).to_f)
             [x1, x2]
           elsif equation_params[:b] != 0 && equation_params[:c] == 0
-            x1 = 0
-            x2 = -equation_params[:b] / equation_params[:a]
+            x1 = 0.to_f
+            x2 = (-equation_params[:b]).to_f / (equation_params[:a]).to_f
             [x1, x2]
           elsif discriminant > 0
-            x1 = (-equation_params[:b] + Math.sqrt(discriminant)) / (2 * equation_params[:a])
-            x2 = (-equation_params[:b] - Math.sqrt(discriminant)) / (2 * equation_params[:a])
+            x1 = ((-equation_params[:b]).to_f + Math.sqrt(discriminant)) / (2 * (equation_params[:a]).to_f)
+            x2 = ((-equation_params[:b]).to_f - Math.sqrt(discriminant)) / (2 * (equation_params[:a]).to_f)
             [x1, x2]
           else discriminant == 0
-            x1 = -equation_params[:b] / (2 * equation_params[:a])
+            x1 = (-equation_params[:b]).to_f / (2 * equation_params[:a])
+            [x1]
+          end
+        end
+
+        def linear_equation(equation_params)
+          if equation_params[:a] == 0 && equation_params[:b] == 0
+            'Infinite'
+          elsif equation_params[:a] == 0 && equation_params[:b] != 0
+            'No solutions'
+          else
+            x1 = (-equation_params[:b]).to_f / (equation_params[:a]).to_f
             [x1]
           end
         end
