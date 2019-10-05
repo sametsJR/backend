f53618b41cbc3c6d22c9fafa5b83987ed9ebdfc9
Alex Samets <aliaksandr.samets@gmail.com>
2019-10-05 14:01:53 +0300

    Add quadratic_equation method to service

--- lib/services/calculates/equation.rb
+++ lib/services/calculates/equation.rb
@@ -1,6 +1,27 @@
 module Services
   module Calculations
     class Equation
+      class << self
+        def call(equation_params)
+          return quadratic_equation(equation_params) if equation_params[:type] == 'quadratic'
+        end
+
+        private
+
+        def quadratic_equation(equation_params)
+          discriminant = equation_params[:b] ** 2 - 4 * equation_params[:a] * equation_params[:c]
+
+          if discriminant > 0
+            x1 = (-equation_params[:b] + Math.sqrt(discriminant)) / (2 * equation_params[:a])
+            x2 = (-equation_params[:b] - Math.sqrt(discriminant)) / (2 * equation_params[:a])
+          elsif discriminant == 0
+            x1 = -equation_params[:b] / (2 * equation_params[:a])
+          else
+            x1 = 'No solutions'
+            x2 = 'No solutions'
+          end
+        end
+      end
     end
   end
 end
